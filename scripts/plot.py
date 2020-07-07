2c3f4971a5bd3b75045bc313539c2ac7713e60c4
jamycheung <jamy.cheung@outlook.com>
2020-07-07 19:45:20 +0200

    plot with color from mapillary

--- scripts/plot.py
+++ scripts/plot.py
@@ -3,6 +3,82 @@ import os
 import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
+# === Colors mapping
+COLORS = [[196, 196, 196],
+       [190, 153, 153],
+       [180, 165, 180],
+       [ 90, 120, 150],
+       [102, 102, 156],
+       [128,  64, 255],
+       [170, 170, 170],
+       [250, 170, 160],
+       [ 96,  96,  96],
+       [230, 150, 140],
+       [128,  64, 128],
+       [110, 110, 110],
+       [244,  35, 232],
+       [150, 100, 100],
+       [ 70,  70,  70],
+       [150, 120,  90],
+       [255, 255, 255],
+       [ 64, 170,  64],
+       [230, 160,  50],
+       [ 70, 130, 180],
+       [190, 255, 255],
+       [152, 251, 152],
+       [107, 142,  35],
+       [  0, 170,  30],
+       [ 70, 100, 150],
+       [  0,  80, 100],
+       [ 32,  32,  32],
+       [120,  10,  10],
+       [165,  42,  42],
+       [  0, 192,   0],
+       [140, 140, 200],
+       [220,  20,  60],
+       [255,   0,   0],
+       [255,   0, 100],
+       [255,   0, 200],
+       [200, 128, 128],
+       [255, 255, 128],
+       [250,   0,  30],
+       [100, 140, 180],
+       [220, 220, 220],
+       [220, 128, 128],
+       [222,  40,  40],
+       [100, 170,  30],
+       [ 40,  40,  40],
+       [ 33,  33,  33],
+       [100, 128, 160],
+       [142,   0,   0],
+       [210, 170, 100],
+       [153, 153, 153],
+       [128, 128, 128],
+       [  0,   0,  80],
+       [250, 170,  30],
+       [192, 192, 192],
+       [220, 220,   0],
+       [140, 140,  20],
+       [119,  11,  32],
+       [150,   0, 255],
+       [  0,  60, 100],
+       [  0,   0, 142],
+       [  0,   0,  90],
+       [  0,   0, 230],
+       [128,  64,  64],
+       [  0,   0, 110],
+       [  0,   0,  70],
+       [  0,   0, 192]]
+
+select_COLORS = {
+    'person': [220,  20,  60],
+    'rider': [255,   0,   0],
+    'pole': [153, 153, 153],
+    'traffic-light': [250, 170,  30],
+    'vehicle': [  0,   0, 142],
+    'bike': [119,  11,  32]
+}
+
 # === dict of instance categories
 INSTANCE = {
     "animal--bird": [],
@@ -78,17 +154,18 @@ SCENCE_FRAME ={
 
 def plot_dict_line(d, frame_ids):
     # --- plot
+    colors = dict((k, np.array(v)/255.0) for k,v in select_COLORS.items())
     def plot_one(sc, fr, id):
         f = plt.figure(figsize=(15, 10))
         for k, v in d.items():
             sub_seq = v[fr[0]:fr[1]]
             plt.plot(range(fr[0], fr[1]), sub_seq,
                      linestyle='-', #marker='o',
-                     label=k.split('--')[-1], alpha=0.7)
+                     label=k.split('--')[-1], color=colors[k], alpha=0.5)
         plt.xlabel('frame')
         plt.ylabel('#instance')
-        # plt.yticks(range(0, max([max(v[fr[0]:fr[1]]) for v in d.values()]), 5))
-        plt.yticks(range(0, max([max(v) for v in d.values()]), 5))
+        plt.yticks(range(0, max([max(v[fr[0]:fr[1]]) for v in d.values()]), 5))
+        # plt.yticks(range(0, max([max(v) for v in d.values()]), 5))
         plt.legend()
         # plt.grid()
         # plt.show()
@@ -121,7 +198,7 @@ def plot_stacked_bar(data, series_labels, category_labels=None,
     for i, row_data in enumerate(data):
         color = colors[i] if colors is not None else None
         axes.append(plt.bar(ind, row_data, bottom=cum_size,
-                            label=series_labels[i], color=color, alpha=0.7))
+                            label=series_labels[i], color=color, alpha=0.5))
         cum_size += row_data
 
     if category_labels:
@@ -152,6 +229,7 @@ def plot_dict_bar(d, frame_ids):
         series_labels = list(d.keys())
         data = [l[fr[0]:fr[1]] for l in d.values()]
         category_labels = range(fr[0], fr[1])
+        colors = [np.array(c)/255.0 for c in select_COLORS.values()]
 
         plot_stacked_bar(
             data,
@@ -159,9 +237,9 @@ def plot_dict_bar(d, frame_ids):
             # category_labels=category_labels,
             # show_values=True,
             # value_format="{:.0f}",
-            # colors=['tab:orange', 'tab:green'],
+            colors=colors,
             x_label="frame",
-            y_label="Instance",
+            y_label="#instance",
             fr=fr
         )
         f.savefig('./plots/'+id+'_ins_frams_'+sc+'_bar.png', bbox_inches='tight')
@@ -216,4 +294,4 @@ if __name__ == '__main__':
     # plot_dict(human, "human_ins.png")
     # plot_dict(obj, "object_ins.png")
     plot_dict_bar(mg_thing, frame_ids)
-    plot_dict_line(mg_thing, frame_ids)
+    # plot_dict_line(mg_thing, frame_ids)

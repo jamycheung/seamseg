c105ca6d3ad7c4099d74c30defac250b9c028561
jamycheung <jamy.cheung@outlook.com>
2020-07-11 16:22:12 +0200

    Add eval_panoptic file

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 train_panoptic.py \
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 eval_panoptic.py \
 --log_dir logs \
 --resume seamseg_r50_vistas.tar \
 --eval \
